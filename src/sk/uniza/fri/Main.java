5b8163c27507b59993ea9146c0f7ed5ce48361b8
Tracktark <rikoczz@gmail.com>
2022-05-17 19:05:40 +0200

    Initial commit

---
+++ src/sk/uniza/fri/Main.java
@@ -0,0 +1,12 @@
+package sk.uniza.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: RZ
+ * Date: 17. 5. 2022
+ * Time: 18:58
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Sach();
+    }
+}
---
+++ src/sk/uniza/fri/Sach.java
@@ -0,0 +1,12 @@
+package sk.uniza.fri;
+
+/**
+ * 17. 5. 2022 - 18:58
+ *
+ * @author RZ
+ */
+public class Sach {
+    public Sach() {
+        System.out.println("Hello world");
+    }
+}
